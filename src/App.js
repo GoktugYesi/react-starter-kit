6c98c2e3f03b58eaabd917897f06334dd70ebf52
Goktug Yesil <goktugbekir13@gmail.com>
2023-02-26 17:18:41 +0300

    JSX adding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import { createElement } from 'react';
+import { createElement, Fragment } from 'react';
 
 import './style.scss'
 
+function Button(props) {
+  return <button>{props.text}</button>
+}
+
 function App() {
   //JSX olmasaydi
-
-  // import { createElement } from 'react'; //bu importu yukarda yaptik
   const todos = ['todo1', 'todo2', 'todo3'];
+  const searchFunction = () => {
+    alert('search');
+  };
+  const name = 'Goktug';
+  const surname = 'Yeşil';
+  
+  // import { createElement } from 'react'; //bu importu yukarda yaptik
+
   const h1 = createElement('h1', null, 'React App');
   const ul = createElement('ul', null, todos.map((todo) => createElement('li', null, todo)));
+  const button = createElement(Button, {
+    text: 'Merhaba Butonu'
+  }, null);
   return createElement('main',{
     className: 'test',
     id: 'main'
-  }, h1, ul)
+  }, h1, ul, button)
+
 
   //JSX ile
+  //camelCase 
+  // Fragment yerine <> </> kullanilabilir  // import { Fragment } from 'react';
   return (
-    <main id='main' className='test'>
-      <h1>React App</h1>
+    <Fragment id='main' className='test'>
+      <Button text='Merhaba Butonu' />
+
+      <h1 style={{color: 'red', backgroundColor: '#ccc'}} tabIndex="1">React App</h1>
+      <label htmlFor='search' tabIndex="3" onClick={searchFunction}>Arama</label>
+      <input type="text" id='search' tabIndex="2"/>
       <ul>
-        {todos.map((todo) => (
-          <li>{todo}</li>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
 
         ))}
       </ul>
-    </main>
+      {name + ' ' + surname === 'Goktug Yeşil'  ? <h1>Goktug Yesil (Admin)</h1> : name + ' ' + surname}
+    </Fragment>
   );
 
 }
