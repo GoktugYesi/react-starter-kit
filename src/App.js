743ecc10a9fc78842005c73b1b67d7e4724db73e
Goktug Yesil <goktugbekir13@gmail.com>
2023-02-26 11:14:58 +0300

    ders2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
+import './style.css';
 
 
 function App() {
+
+  if (process.env.NODE_ENV === 'production') {
+    //analtics bilgi gönderimi
+  }
+
   return (
     <div className="App">
-      test
-      <img src={logo} alt="logo"  width={252} height={252} />
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      {process.env.NODE_ENV === 'production' && 
+      <img src="/logo192.png" alt="" />
+      }
     </div>
   );
 }
